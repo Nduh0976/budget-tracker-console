6959ef69c145000d0f283e687b33f370479d811b
Ndumiso <ndumisomanqele7@gmail.com>
2025-05-28 21:21:07 +0200

    36: Clear terminal when running the first time

--- BudgetTracker.Console/Program.cs
+++ BudgetTracker.Console/Program.cs
@@ -1,6 +1,18 @@
 ﻿using BudgetTracker.Console;
 using BudgetTracker.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+// Clear terminal
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    Process.Start("cmd", "/c cls").WaitForExit();
+}
+else
+{
+    Process.Start("clear").WaitForExit();
+}
 
 var serviceProvider = AppInitializer.InitializeServices();
 
